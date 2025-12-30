0cfc7b09fb888a0b23a95544f09710fdfd5f00fd
hibohiboo <hibohiboo66+github@gmail.com>
2025-12-30 22:52:31 +0900

    fix layout

--- apps/frontend/src/page/top/ui/Page.tsx
+++ apps/frontend/src/page/top/ui/Page.tsx
@@ -2,6 +2,12 @@ import { Link } from 'react-router';
 
 export const Page = () => (
   <div>
+    <p style={{ marginBottom: '1rem' }}>
+      <a href="https://lostrpg-751c1.firebaseapp.com/lost/" target="_blank">
+        ルールブック
+      </a>
+    </p>
+
     <ul>
       <li>
         <Link to="/camp">キャンプ</Link>
